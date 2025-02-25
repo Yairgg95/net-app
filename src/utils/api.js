ef6c894266483aabcbd253d96364f1256b0c8a79
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-02-25 12:09:13 -0600

    [FEAT] Add fetch functions to get User and Posts

---
+++ src/utils/api.js
@@ -0,0 +1,34 @@
+const URL = "https://jsonplaceholder.typicode.com";
+
+export default async function getPosts() {
+  try {
+    const res = await fetch(`${URL}/posts`);
+    const data = res.json();
+    return data
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+export default async function getUser(id) {
+    try {
+        const res = await fetch(`${URL}/users/${id}`)
+        const data = res.json();
+        return data
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+export default async function getUserPosts(userId = 1) {
+    try {
+        // usando un query para buscar los post del usuario 1
+    const res = await fetch(`${URL}/posts?userId=${userId}`)
+    const data = res.json();
+    return data
+    } catch (error) {
+        console.error(error);
+    }
+}
